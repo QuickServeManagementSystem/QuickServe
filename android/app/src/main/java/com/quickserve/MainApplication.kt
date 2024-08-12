2d7c61339bb77a71deea0d7b6712bb111a99c065
Hoang Phi Khanh <phikhanh0169@gmail.com>
2024-08-12 17:29:30 +0700

    feat: zalo pay sandbox

--- android/app/src/main/java/com/quickserve/MainApplication.kt
+++ android/app/src/main/java/com/quickserve/MainApplication.kt
@@ -10,6 +10,9 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
+import com.quickserve.zpmodule.PayZaloBridge;
+import vn.zalopay.sdk.Environment
+import vn.zalopay.sdk.ZaloPaySDK
 
 class MainApplication : Application(), ReactApplication {
 
@@ -19,6 +22,7 @@ class MainApplication : Application(), ReactApplication {
             PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
               // add(MyReactNativePackage())
+              add(PayZaloBridge())
             }
 
         override fun getJSMainModuleName(): String = "index"
@@ -35,6 +39,7 @@ class MainApplication : Application(), ReactApplication {
   override fun onCreate() {
     super.onCreate()
     SoLoader.init(this, false)
+    ZaloPaySDK.init(2553, Environment.SANDBOX);
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
